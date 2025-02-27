b0d6c4e0c8a0ae33cbb451bfdc70778b56701483
Satyam Gupta <satyam.bn6@gmail.com>
2025-02-27 22:49:24 +0530

    Prisma migration

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,12 +1,30 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
   const { userId, zapId } = req.params;
   const { data } = req.body;
+  await prisma.$transaction(async (tx) => {
+    const zaprun = await tx.zaprun.create({
+      data: {
+        zapId: zapId,
+        metadata: data,
+      },
+    });
+
+    await tx.zaprunOutbox.create({
+      data: {
+        zaprunId: zaprun.id,
+      },
+    });
+  });
+
   console.log(data);
   res.send("ok");
 });
