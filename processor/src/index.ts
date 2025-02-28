fac3dc2770fa978ce6a9fb9c5592074eff50d507
Satyam Gupta <satyam.bn6@gmail.com>
2025-02-28 22:15:35 +0530

    Added processor for Kafka producer

---
+++ processor/src/index.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+import {Kafka} from "kafkajs";
+const prisma = new PrismaClient();
+
+const TOPIC_NAME = "zap-events"
+const kafka = new Kafka({
+    clientId: "outbox-processor",
+    brokers: ["localhost:9092"]
+})
+
+
+async function main() {
+    const producer = kafka.producer()
+    await producer.connect()
+
+    while(1)
+        {
+            const pendingZapruns = await prisma.zaprunOutbox.findMany({
+                where: {},
+                take:10
+            })
+
+            
+            producer.send({
+                topic: TOPIC_NAME,
+                messages: pendingZapruns.map((zaprun) => ({
+                    value: zaprun.zaprunId
+                }))
+            })
+            
+            await prisma.zaprunOutbox.deleteMany({
+                where: {
+                    id: {
+                        in: pendingZapruns.map((zaprun) => zaprun.id)
+                    }
+                }
+            })
+            
+        }
+}
+
+
+main()
