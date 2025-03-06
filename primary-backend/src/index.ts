9eee98efc65f673c8fcaa7105d4d71583902b5bd
Satyam Gupta <satyam.bn6@gmail.com>
2025-03-06 21:09:12 +0530

    Backend and hooks URL

--- primary-backend/src/index.ts
+++ primary-backend/src/index.ts
@@ -11,6 +11,6 @@ app.use("/api/v1/user", userRouter);
 
 app.use("/api/v1/zap", zapRouter);
 
-app.listen(3000, () => {
+app.listen(3000 , () => {
     console.log("Server is running on port 3000");
 });
