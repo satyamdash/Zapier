6c6119c13de9a1eac20eedbc72b88e879c755a9e
Satyam Gupta <satyam.bn6@gmail.com>
2025-03-06 21:18:53 +0530

    Resolved warning

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -8,12 +8,12 @@ export const Hero = () => {
     const router = useRouter();
     return <div>
         <div className="flex justify-center">
-            <div className="text-5xl font-bold font-semibold text-center pt-8 max-w-xl">
+            <div className="text-5xl font-bold text-center pt-8 max-w-xl">
                 Automate as fast as you can type    
             </div>
         </div>
         <div className="flex justify-center pt-2">
-            <div className="text-xl font-bold font-normal text-center pt-8 max-w-2xl">
+            <div className="text-xl font-bold text-center pt-8 max-w-2xl">
                 AI gives you automation superpowers, and Zapier puts them to work. Pairing AI and Zapier helps you turn ideas into workflows and bots that work for you.
             </div>
         </div>
